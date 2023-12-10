3bc8fe36dc6c0f02d0c900295c4935a782bf86db
aryankad1an <aryankaddian842@gmail.com>
2023-12-11 00:16:45 +0530

    module 1 commit

--- src/module01.java
+++ src/module01.java
@@ -1,5 +1,12 @@
 public class module01 {
+    // java is a platform independent language, it just needs JVM to run
+    // JVM(java virtual machine) is available for multiple OS but not iPhone lol
+    // JVM understands only byte code, but we create Java Code which is converted to byte code by the compiler
+    // JVM needs the first file that needs to be executed which must have a "main" method
+    // A class is must for java code execution
+    // JVM is a part of JRE(Java Runtime Environment)
+    // JRE is a part of JDK(Java Development Kit)
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("This is the beginning!");
     }
 }
