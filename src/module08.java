ee23b6b0e04e90fb8c62988705bab89bb819c95d
aryankad1an <aryankaddian842@gmail.com>
2023-12-13 17:42:52 +0530

    arrays commit

--- src/module08.java
+++ src/module08.java
@@ -1,3 +1,16 @@
 public class module08 {
+    public static void main(String[] args){
+        // arrays in Java
 
+        int[] _nums = {3, 4, 5, 6};  // <-- java style array declaration
+        _nums[0] = 4;
+        System.out.println(_nums[0]);
+
+        int[] _nums_2 = new int[4]; // all the 4 values are zero
+        System.out.println(_nums_2[0]);
+
+        // multi dimentional arrays
+
+
+    }
 }
