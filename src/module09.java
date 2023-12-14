240390867e28790ae8e2e3a709efa795c17723c2
aryankad1an <aryankaddian842@gmail.com>
2023-12-14 21:34:28 +0530

    strings commit

--- src/module09.java
+++ src/module09.java
@@ -22,5 +22,27 @@ public class module09 {
         students[0].rollno = 4;
         System.out.println(s1.rollno);
 
+
+        // Strings in JAVA
+        // String is a class in java
+
+        // creating string as object lmao
+        String name = new String("aryan");
+
+
+        String fname = "nig";
+        String sname = "nig"; // fact: the two strings are at same location in the heap memory
+        // references fname and sname are refering to the same memory location
+        System.out.println(name);
+        System.out.println(fname);
+
+        System.out.println(fname+"ga"); // works
+
+        // modifying the string
+        name = "arun";
+        // strings are stored in "String Constant Pool" in heap memory
+        // when the string is modified, the original value is not changed
+        // but the reference is shifted from one place to a new object
+        System.out.println(name);
     }
 }
