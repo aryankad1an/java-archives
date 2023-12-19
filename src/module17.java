c269bc459e0c046bddba6e7471eef22520769999
aryankad1an <aryankaddian842@gmail.com>
2023-12-19 18:08:16 +0530

    need of inheritance

--- src/module17.java
+++ src/module17.java
@@ -1,5 +1,9 @@
 public class module17 {
     public static void main(String[] args) {
-
+        /*
+        NEED OF INHERITANCE:
+            an object "is" some other object, in that case we use inheritance
+            eg: a laptop is a computer, a human is an animal
+        */
     }
 }
