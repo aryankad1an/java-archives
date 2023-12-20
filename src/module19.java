b0ca5b260d7d0966fd3ffa04401a08e3ddd2870e
aryankad1an <aryankaddian842@gmail.com>
2023-12-20 11:13:56 +0530

    packages

---
+++ src/module19.java
@@ -0,0 +1,8 @@
+import tools.Screw;
+
+public class module19 {
+    public static void main(String[] args) {
+        Screw s = new Screw();
+        s.add();
+    }
+}
---
+++ src/tools/Screw.java
@@ -0,0 +1,7 @@
+package tools;
+// used in module 19
+public class Screw {
+    public void add(){
+        System.out.println("Screw Added!");
+    }
+}
