7bd96379b99c79b810556436a729558ccb6d54e2
aryankad1an <aryankaddian842@gmail.com>
2023-12-20 21:03:49 +0530

    dynamic method dispatch

--- src/module20.java
+++ src/module20.java
@@ -1,6 +1,16 @@
 class Marksheet{
     int marks = 6; // default variable
+    public void show(){
+        System.out.println("In marksheet");
+    }
 }
+
+class Grades extends Marksheet{
+    public void show(){
+        System.out.println("In grades");
+    }
+}
+
 public class module20 {
     public static void main(String[] args) {
         Marksheet m = new Marksheet();
@@ -18,7 +28,22 @@ public class module20 {
         // polymorphism at compile time is method overloading
         // polymorphism at run time is method overriding (common sense)
 
+        // illustrating runtime polymorphism with dynamic method dispatch
 
+
+        Marksheet obj = new Marksheet();
+
+        // taking reference of parent class and object of child class
+        Marksheet obj2 = new Grades(); // this works because marksheet is a grades but grades is not marksheet lol
+        // Grades obj = new Marksheet(); <-- this doesnt work
+
+        obj.show();
+        obj2.show(); // this is in grade as reference is to grade
+
+        obj = new Grades(); // object reference reassigned
+        obj.show();
+
+        // what obj.show will show is decided on runtime, thats why its runtime polymorphism
     }
 }
 
