3b428b6b7f0bb5ebb05736af3518a64ed69f9f84
aryankad1an <aryankaddian842@gmail.com>
2023-12-23 13:21:07 +0530

    inner class

---
+++ src/module24.java
@@ -0,0 +1,34 @@
+class Mouse{
+    // class inside class is inner class
+    class TrackBall{
+        public void config(){
+            System.out.println("in config");
+        }
+    }
+
+    static class StaticInnerClass{
+        public void show(){
+            System.out.println("in static inner class");
+        }
+    }
+    public void show(){
+        System.out.println("in show");
+    }
+}
+
+
+public class module24 {
+    public static void main(String[] args) {
+        // Inner class in Java
+        Mouse mice1 = new Mouse();
+        mice1.show();
+
+        Mouse.TrackBall ball1 = mice1.new TrackBall(); // proper syntax of making an inner class
+        ball1.config();
+
+        // syntax for making static class
+        Mouse.StaticInnerClass c = new Mouse.StaticInnerClass();
+        c.show();
+
+    }
+}
