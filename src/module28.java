3e2067cacdc35f7ded735373e5a978af23b5318c
aryankad1an <aryankaddian842@gmail.com>
2023-12-23 14:20:06 +0530

    lambda expression

--- src/module28.java
+++ src/module28.java
@@ -1,17 +1,25 @@
 @FunctionalInterface
 interface I1{
-    void show();
+    void show(int i);
     // functional interfaces are those interfaces with only one function
 }
 class A1 implements I1{
-    public void show(){
+    public void show(int i){
         System.out.println("In show");
+        System.out.println(i);
     }
 }
 
 public class module28 {
     public static void main(String[] args) {
         A1 a1 = new A1();
-        a1.show();
+        a1.show(69);
+
+        // lambda expression in Java
+        I1 obj = (i) -> {
+            System.out.println("in show(lambda way)");
+            System.out.println(i);
+        };
+        obj.show(70);
     }
 }
