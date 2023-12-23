09cc933be8273dc6af937a30eab81eef34e9e13b
aryankad1an <aryankaddian842@gmail.com>
2023-12-23 20:37:10 +0530

    exceptions update

--- src/module29.java
+++ src/module29.java
@@ -3,11 +3,14 @@ public class module29 {
         // marker interface
         // it is an interface with zero methods(in detail later)
 
-        // exceptions
+        // Errors
 
         // 1. Compile Time Error: Error during compilation
-        // 2. Run Time Error: Error during the execution of application(eg. dividing by 0)
+        // 2. Run Time Error(Exceptions): Error during the execution of application(eg. dividing by 0)
         // 3. Logical Error: Human's skill issue in making logic :/ (hardest to solve)
+
+        // Try and Catch method of handling exceptions
+
 
     }
 }
