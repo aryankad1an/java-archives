90a96cdd91657160ba146298df3fa564429d0606
aryankad1an <aryankaddian842@gmail.com>
2023-12-23 13:42:23 +0530

    more on interfaces

--- src/module25.java
+++ src/module25.java
@@ -1,5 +1,10 @@
 interface Comp{
+    int model = 44; // all the variables in interfaces are final(constant) and static
     void code();
+}
+
+interface Bluetooth{
+    void connect();
 }
 
 class Developer{
@@ -8,9 +13,13 @@ class Developer{
     }
 }
 
-class Laptop implements Comp{
+class Laptop implements Comp, Bluetooth{
     public void code(){
         System.out.println("Coding..");
+    }
+
+    public void connect() {
+        System.out.println("Connected to bluetooth");
     }
 }
 
@@ -31,6 +40,12 @@ public class module25 {
 
         Developer n = new Developer();
         n.devApp(rog);
+
+        // implementing multiple interfaces
+        Laptop lap = new Laptop();
+        lap.connect();
+
+        // note that interface-interface inheritance can be facilitated by "extends"
 
     }
 }
