783cab927f993eb6345d9f975f4cd64d5d2f3b42
aryankad1an <aryankaddian842@gmail.com>
2023-12-23 13:47:43 +0530

    intro to enums

---
+++ src/module26.java
@@ -0,0 +1,21 @@
+enum Status{
+    Running, Failed, Pending, Success;
+}
+public class module26 {
+    public static void main(String[] args) {
+
+        // enums in java
+        Status isRunning = Status.Running;
+        Status isDownloading = Status.Pending;
+        System.out.println(isRunning);
+        System.out.println(isRunning.ordinal()); // every status has a number assigned to it(like index)
+        // this is kind of our custom cute data type :3
+
+
+        Status[] allStatus = Status.values();
+        for (Status s: allStatus) {
+            System.out.println(s);
+        }
+
+    }
+}
